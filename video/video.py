04d030ba5b34333d95d81dd9acd96a7b8ae7d925
LandryBulls <bulls409@gmail.com>
2023-09-18 15:05:06 -0400

    updating stuff

--- video/video.py
+++ video/video.py
@@ -5,7 +5,7 @@ import subprocess
 def concatenate(cam_directory):
     cam_identity = os.path.basename(cam_directory)
     camera_path = Path(cam_directory)
-    to_concat = [i for i in camera_path.iterdir() if Path(i).suffix.lower() == '.mp4']
+    to_concat = [i for i in camera_path.iterdir() if Path(i).suffix.lower() == '.mp4'].sort()
     add_text = camera_path / 'concat_history.txt'
 
     with open(add_text, "w") as f:
@@ -14,7 +14,7 @@ def concatenate(cam_directory):
 
     # just put the .mp4's in the same directory, then put the trimmed files in derivatives later
     filename = f'{cam_identity}_concatenated.mp4'
-    concat_command = (f'ffmpeg -f concat -safe 0 -i {str(add_text)} -c copy {filename}')
+    concat_command = (f'ffmpeg -f concat -safe 0 -i {str(add_text)} -c copy {camera_path / filename}')
 
     print(f"Concatenating {cam_identity}...")
     subprocess.run(concat_command, shell=True)
@@ -27,7 +27,7 @@ def to_equirectangluar(cam_directory):
         filename = os.path.basename(file)[:-4]
         output_name = str(cam_directory / f'{filename}_equirectangular.mp4')
         command = f"ffmpeg -hwaccel opencl -v verbose -filter_complex '[0:0]format=yuv420p,hwupload[a] , [0:5]format=yuv420p,hwupload[b], [a][b]gopromax_opencl, hwdownload,format=yuv420p' -i {str(file)} -c:v libx264 -pix_fmt yuv420p -map_metadata 0 -map 0:a -map 0:3 {output_name}"
-        subprocess.run(command, shell=True)
+        subprocess.run(command, shell=True, stdout=open(os.devnull, 'wb'))
 
 
 def apply_concatenation(data_dir):
