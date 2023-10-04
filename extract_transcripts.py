6d24ec9226d22d1945073a987fb66790f72e67bb
LandryBulls <bulls409@gmail.com>
2023-10-04 16:30:33 -0400

    add script for getting transcripts given data dir

---
+++ extract_transcripts.py
@@ -0,0 +1,29 @@
+from voicolate.isolate import isolate
+from voicolate.transcribe import transcribe
+from pathlib import Path
+import joblib
+
+letter_assignments = ['A', 'B', 'C', 'D']
+
+def extract_transcripts(data_dir):
+    data_dir = Path(data_dir)
+    derivative_path = data_dir / 'derivatives'
+    audio_files = [str(i) for i in derivative_path.iterdir() if Path(i).suffix == '.wav']
+    audio_files.sort()
+    processed_path = data_dir / 'processed'
+    # make dir
+    processed_path.mkdir(exist_ok=True)
+    # isolate
+    isolated_files = isolate(audio_files, save_files=True, output_path=str(processed_path))
+    isolated_files.sort()
+    # transcribe
+    transcripts = []
+    for file in isolated_files:
+        transcript = transcribe(file)
+        transcripts.append(transcript)
+    # save transcripts
+    for letter, transcript in zip(letter_assignments, transcripts):
+        joblib.dump(transcript, processed_path / f'{letter}_transcript.joblib')
+
+
+
